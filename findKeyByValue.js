ee5dc07c03ac081f69839124d8f7cc7b4ac80869
Eghosa <perezonline300@gmail.com>
2023-11-18 11:05:04 -0500

    update

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+// Copy the assertEqual function here
+const assertEqual = function(actual, expected) {
+  // Implementation of assertEqual
+};
+
+// Function to implement
+const findKeyByValue = function(object, value) {
+  for (const key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+// Test cases
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
