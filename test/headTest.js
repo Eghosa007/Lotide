eede85e3499f5057a25dc2ca6ccb76cbdfdeea98
Eghosa <perezonline300@gmail.com>
2023-11-21 16:13:33 -0500

    update

--- test/headTest.js
+++ test/headTest.js
@@ -1,17 +1,32 @@
 // test/headTest.js
 
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+//const head = require('../head');
+//const assertEqual = require('../assertEqual');
 
 // TEST CASES
 // Test Case 1: Check if head returns the first element of the array
-assertEqual(head([5, 6, 7]), 5);
+//assertEqual(head([5, 6, 7]), 5);
 
 // Test Case 2: Check if head returns the first string element of the array
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
 // Test Case 3: Check if head returns the only element in the array
-assertEqual(head([42]), 42);
+//assertEqual(head([42]), 42);
 
 // Test Case 4: Check if head returns undefined for an empty array
-assertEqual(head([]), undefined);
+//assertEqual(head([]), undefined);
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
+
+it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+
